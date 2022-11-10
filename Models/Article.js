20e997986bd9c87fbf6644c4b0d95b85850f3045
Kelum Ayuwardhana <kelumayuwardhana@gmail.com>
2022-11-10 23:21:13 +0530

    Article backend updated

--- Models/Article.js
+++ Models/Article.js
@@ -5,21 +5,25 @@ const ArticleSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    imageURL: {
+    img: {
         type: String,
     },
     description: {
         type: String,
         required: true,
     },
-    authors: [{
+    author: {
         type: String,
         required: true,
-    }],
+    },
     createdDate: {
         type: Date,
         default: Date.now,
     },
+    status:{
+        type: String,
+        default: 'Draft',
+    }
 });
 
 const Article = mongoose.model(
